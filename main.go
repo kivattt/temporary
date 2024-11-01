be6974eb2cc2b1649d456a96e2601b3a113da923
kivattt <kivattt@gmail.com>
2024-11-01 13:24:04 +0100

    h

--- main.go
+++ main.go
@@ -10,6 +10,22 @@ func main() {
 	w, _ := fsnotify.NewWatcher()
 	defer w.Close()
 
+	go func() {
+		defer fmt.Println("Channel handler quit!")
+		for {
+			select {
+			case _, ok := <-w.Events:
+				if !ok {
+					return
+				}
+			case _, ok := <-w.Errors:
+				if !ok {
+					return
+				}
+			}
+		}
+	}()
+
 	fmt.Println("Adding .")
 	w.Add(".")
 	fmt.Println("Removing .")
